9c6238f17ab2c34bf8ac8ffa5f2f6a71ca5a1c0b
Carl Shan <shan.carl@gmail.com>
2015-04-17 15:36:10 -0500

    Changing version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='datascience_tools',
-    version='0.0.1',
+    version='0.0.2',
     description='Tools for data science',
     author='Carl Shan',
     author_email='carl@carlshan.com',
